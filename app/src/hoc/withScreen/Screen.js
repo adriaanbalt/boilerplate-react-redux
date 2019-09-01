1352479092bcd6211b37ef2ee3364772e861063a
adriaanbalt <adriaan@balt.us>
2019-09-01 17:43:38 -0400

    updating file name for withScreen styles to include module so it works as a css module

--- app/src/hoc/withScreen/Screen.js
+++ app/src/hoc/withScreen/Screen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './styles.scss'
+import styles from './styles.module.scss'
 import classnames from 'classnames'
 import IS_MOBILE from '../../lib/is-mobile'
 
