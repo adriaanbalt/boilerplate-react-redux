5968e1cafefa188874fa8312058be1067a64eac9
adriaanbalt <adriaan@balt.us>
2019-09-01 12:56:53 -0400

    comment in analytics middleware

--- app/src/middleware/analytics.js
+++ app/src/middleware/analytics.js
@@ -1,3 +1,5 @@
 export default store => next => action => {
+    // for specific actions, run analytics calls to the end point
+    // useful to consolidate analytics into a single place as opposed to spreading it across the app
     return next(action)
 }
