33f553bf316669845ed6648bf5b6d7b0979b168c
adriaanbalt <adriaan@balt.us>
2019-09-01 13:07:22 -0400

    blog list view references renamed styles.module.scss file and uses connect to access reducer data

--- app/src/BlogListView/index.js
+++ app/src/BlogListView/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './styles.module.scss';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
-import styles from './style.module.scss';
 
-function App() {
-    return (
-        <div className={styles["BlogListView"]}>
-            <h1>Blog List View</h1>
-            <Link to="/details/2">Go to details</Link>
-        </div>
-    );
+const mapStateToProps = state => ({
+    posts: state.PostsReducer.posts,
+})
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+}, dispatch)
+
+/**
+ * @class App
+ * @extends {Component}
+ */
+class BlogListView extends Component {
+
+    render() {
+        console.log('blog list view', this.props)
+        return (
+            <div className={styles["BlogListView"]}>
+                <h1>List View</h1>
+                <Link to="/">Home</Link>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(BlogListView);
