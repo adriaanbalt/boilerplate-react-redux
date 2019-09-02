4339e03c7fe45ca6ec80415805e20f19b52ca6a9
adriaanbalt <adriaan@balt.us>
2019-09-02 19:31:59 -0400

    updating app and i18n tests

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store, { history } from './Store';
+import { ConnectedRouter } from 'react-router-redux'
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.Fragment>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </React.Fragment>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
