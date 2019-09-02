627b028aedbd95dde80b96362fcabebe8f129ea1
adriaanbalt <adriaan@balt.us>
2019-09-02 19:31:02 -0400

    adding dummy analytics events

--- app/src/middleware/analytics.js
+++ app/src/middleware/analytics.js
@@ -1,5 +1,43 @@
+import {
+    TOGGLE_FAVORITE,
+    SELECT_SORT_OPTION,
+    PERFORM_SEARCH,
+} from '../actions'
+
+/**
+ * This wrapper function sends the data to the server
+ * @param {Object} data the event data to send
+ */
+const sendEvent = (data, ml = null) => {
+    // Analytics.track({
+    //     data,
+    // })
+}
+
 export default store => next => action => {
     // for specific actions, run analytics calls to the end point
     // useful to consolidate analytics into a single place as opposed to spreading it across the app
+
+    if (action.type === TOGGLE_FAVORITE) {
+        sendEvent({
+            name: "toggle-favorite",
+            id: action.id,
+        })
+    }
+
+    if (action.type === SELECT_SORT_OPTION) {
+        sendEvent({
+            name: "select-sort",
+            id: action.selected,
+        })
+    }
+
+    if (action.type === PERFORM_SEARCH) {
+        sendEvent({
+            name: "perform-search",
+            id: action.query,
+        })
+    }
+
     return next(action)
 }
